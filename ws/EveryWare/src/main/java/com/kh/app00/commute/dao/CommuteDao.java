3e0a54de377ee5d47268a5e38a80d665f7249f4e
AREUM\lee99 <lee990103@naver.com>
2022-10-19 21:15:25 +0900

    시간 외 근무 신청

--- ws/EveryWare/src/main/java/com/kh/app00/commute/dao/CommuteDao.java
+++ ws/EveryWare/src/main/java/com/kh/app00/commute/dao/CommuteDao.java
@@ -3,10 +3,14 @@ package com.kh.app00.commute.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.kh.app00.commute.vo.CommuteVo;
+import com.kh.app00.commute.vo.OverworkVo;
 
 public interface CommuteDao  {
 
     //출퇴근 체크
     int insertCommute(SqlSessionTemplate sst, CommuteVo vo);
+
+    //시간 외 근무 신청
+    int insertOver(SqlSessionTemplate sst, OverworkVo vo);
 
 }
