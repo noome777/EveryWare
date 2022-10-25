e564692ba3d6e623bea5609d2ec41942a7ebce81
AREUM\lee99 <lee990103@naver.com>
2022-10-25 23:06:24 +0900

    휴가
    1. 휴가 현황
    2. fullcalendar 사용하여 같은 부서에 속한 사원의 결재 승인 완료된 데이터 조회
    
    (cherry picked from commit 7457e9895a17cfdf3fbd42789ff09821ca9cd45a)
    (cherry picked from commit 348541843b7c765ff0fdf212390e38d4c558072f)

--- ws/EveryWare/src/main/java/com/kh/app00/dayoff/controller/DayoffController.java
+++ ws/EveryWare/src/main/java/com/kh/app00/dayoff/controller/DayoffController.java
@@ -2,6 +2,7 @@ package com.kh.app00.dayoff.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.app00.common.PageVo;
 import com.kh.app00.common.Pagination;
@@ -24,191 +26,193 @@ import com.kh.app00.organization.vo.RightVo;
 @Controller
 @RequestMapping("dayoff")
 public class DayoffController {
-    
+
     private final DayoffService service;
 
     @Autowired
     public DayoffController(DayoffService service) {
         this.service = service;
     }
-    
-    
-    //휴가 신청 화면 && 리스트 조회
+
+    // 휴가 신청 화면 && 리스트 조회
     @GetMapping("main/{pno}")
-    public String main(DayoffVo vo, Model model, HttpSession session, 
+    public String main(DayoffVo vo, Model model, HttpSession session,
             @PathVariable int pno, String offStartDate, String offEndDate) {
-        
-        //로그인 유저의 정보 vo에 저장
-        EmpVo loginMember = (EmpVo)session.getAttribute("loginMember");
+
+        // 로그인 유저의 정보 vo에 저장
+        EmpVo loginMember = (EmpVo) session.getAttribute("loginMember");
         vo.setECode(loginMember.getEmpCode());
         vo.setDeptCode(loginMember.getDeptCode());
-        
-        //만약, 로그인 유저가 관리자, 결재 관리자, 인사 관리자 등등이면 다른 페이지 보여주기 (RIGHT_CODE가 4가 아닐 때)
-        //RIGHT_CODE 조회하기
+
+        // 만약, 로그인 유저가 관리자, 결재 관리자, 인사 관리자 등등이면 다른 페이지 보여주기 (RIGHT_CODE가 4가 아닐 때)
+        // RIGHT_CODE 조회하기
         EmpVo rvo = service.selectRightVo(vo);
-        
-        if(!rvo.getRightCode().equals("4")) {
+
+        if (!rvo.getRightCode().equals("4")) {
             return "redirect:/dayoff/admin/1";
         }
-        
-        //휴가 현황 
+
+        // 휴가 현황
         int offTotalCnt = service.offTotalCnt(vo);
         int offUsedCnt = service.offUsedCnt(vo);
         int offLeftCnt = offTotalCnt - offUsedCnt;
-        
-        //기간 선택 조회 여부(휴가 메인의 전체리스트 || 기간 선택 후 리스트)
-        if(offStartDate != null && offEndDate != null) {
-            //기간 선택을 했을 경우
-            //사원의 기간 선택 목록 조회 (+페이징)
+
+        // 기간 선택 조회 여부(휴가 메인의 전체리스트 || 기간 선택 후 리스트)
+        if (offStartDate != null && offEndDate != null) {
+            // 기간 선택을 했을 경우
+            // 사원의 기간 선택 목록 조회 (+페이징)
             vo.setOffStartDate(offStartDate);
             vo.setOffEndDate(offEndDate);
-            
-            //기간 선택 후 신청글 수 조회
+
+            // 기간 선택 후 신청글 수 조회
             int dateCount = service.selectDateCnt(vo);
             PageVo pv2 = Pagination.getPageVo(dateCount, pno, 5, 5);
-            
+
             List<DayoffVo> dateList = service.selectDateList(vo, pv2);
-            
+
             model.addAttribute("dateList", dateList);
             model.addAttribute("vo", vo);
             model.addAttribute("pv", pv2);
             model.addAttribute("dateCount", dateCount);
-        }else {
-            //기간 선택을 하지 않았을 경우
-            //리스트의 전체 신청글 수 조회
+        } else {
+            // 기간 선택을 하지 않았을 경우
+            // 리스트의 전체 신청글 수 조회
             int listCount = service.selectTotalCnt(vo);
             PageVo pv1 = Pagination.getPageVo(listCount, pno, 5, 5);
-            
+
             List<DayoffVo> voList = service.dayoffList(vo, pv1);
-            
+
             model.addAttribute("voList", voList);
             model.addAttribute("pv", pv1);
             model.addAttribute("listCount", listCount);
         }
-        
+
         model.addAttribute("offTotalCnt", offTotalCnt);
         model.addAttribute("offUsedCnt", offUsedCnt);
         model.addAttribute("offLeftCnt", offLeftCnt);
-        
+
         return "dayoff/dayoffMain";
     }
-    
 
-    //휴가 신청
+    // 휴가 신청
     @PostMapping("main/{pno}")
-    public String insertOff(DayoffVo vo, HttpSession session, 
+    public String insertOff(DayoffVo vo, HttpSession session,
             String offStartDate, String offEndDate, @PathVariable int pno) {
 
-        //로그인 유저 정보 vo에 저장
-        EmpVo loginMember = (EmpVo)session.getAttribute("loginMember");
+        // 로그인 유저 정보 vo에 저장
+        EmpVo loginMember = (EmpVo) session.getAttribute("loginMember");
         vo.setECode(loginMember.getEmpCode());
-        
-        //사원의 부서 정보 vo에 저장
+
+        // 사원의 부서 정보 vo에 저장
         DayoffVo deptVo = service.getDeptVo(vo);
         vo.setDeptCode(deptVo.getDeptCode());
-        
-        if(offStartDate.length() == 21 && offEndDate.length() == 21) {
+
+        if (offStartDate.length() == 21 && offEndDate.length() == 21) {
             vo.setOffStartDate(offStartDate.substring(11));
             vo.setOffEndDate(offEndDate.substring(11));
         }
-        
-        //DB에 신청 정보 insert
+
+        // DB에 신청 정보 insert
         int result = service.insertOff(vo);
-        
-        if(result == 1) {
+
+        if (result == 1) {
             session.setAttribute("alertMsg", "휴가 신청이 완료 되었습니다.");
             return "redirect:/dayoff/main/1";
         } else {
             session.setAttribute("alertMsg", "신청에 실패하셨습니다.");
             return "dayoff/dayoffMain";
         }
-        
-        
+
     }
-    
-    //휴가의 관리자 페이지
+
+    // 휴가의 관리자 페이지
     @GetMapping("admin/{pno}")
-    public String dayoffAdmin(@PathVariable int pno, Model model, DayoffVo vo, 
-            String offStartDate, String offEndDate){
-        
-        //기간 선택 조회 여부(휴가 메인의 전체리스트 || 기간 선택 후 리스트)
-        if(offStartDate != null && offEndDate != null) {
-            //기간 선택을 했을 경우
-            //기간 선택 목록 조회 (+페이징)
+    public String dayoffAdmin(@PathVariable int pno, Model model, DayoffVo vo,
+            String offStartDate, String offEndDate) {
+
+        // 기간 선택 조회 여부(휴가 메인의 전체리스트 || 기간 선택 후 리스트)
+        if (offStartDate != null && offEndDate != null) {
+            // 기간 선택을 했을 경우
+            // 기간 선택 목록 조회 (+페이징)
             vo.setOffStartDate(offStartDate);
             vo.setOffEndDate(offEndDate);
-            
-            //기간 선택 후 신청글 수 조회
+
+            // 기간 선택 후 신청글 수 조회
             int adDateCount = service.selectAdDateCnt(vo);
             PageVo pv2 = Pagination.getPageVo(adDateCount, pno, 5, 10);
-            
+
             List<DayoffVo> AdDateList = service.selectAdDateList(vo, pv2);
-            
+
             model.addAttribute("AdDateList", AdDateList);
             model.addAttribute("vo", vo);
             model.addAttribute("pv", pv2);
             model.addAttribute("adDateCount", adDateCount);
-        }else {
-            //기간 선택을 하지 않았을 경우
-            //휴가 결재를 위한 전체 목록 조회 (+ 페이징)
+        } else {
+            // 기간 선택을 하지 않았을 경우
+            // 휴가 결재를 위한 전체 목록 조회 (+ 페이징)
             int adListCount = service.selectAdminTotalCnt();
             PageVo pv = Pagination.getPageVo(adListCount, pno, 5, 10);
-            
+
             List<DayoffVo> voList = service.adminDayoffList(pv);
-            
+
             model.addAttribute("adListCount", adListCount);
             model.addAttribute("pv", pv);
             model.addAttribute("voList", voList);
         }
-        
+
         return "dayoff/adminDayoff";
-       
+
     }
-    
-    //관리자의 결재 상태 반영
+
+    // 관리자의 결재 상태 반영
     @PostMapping("sendApproval")
     public String sendApproval(String num, String approval, DayoffVo vo, Model model) {
-        
-        //결재 상태 바꾸는 게시글 setting 해주기
- 
-        approval = approval.substring(approval.length()-1, approval.length());
+
+        // 결재 상태 바꾸는 게시글 setting 해주기
+
+        approval = approval.substring(approval.length() - 1, approval.length());
 
         vo.setOffCode(num);
         vo.setOffApproval(approval);
-        
+
         System.out.println(vo);
         int result = service.updateApproval(vo);
-        
-        
-        if(result == 1) {
+
+        if (result == 1) {
             return "redirect:/dayoff/admin/1";
-        }else {
+        } else {
             model.addAttribute("alertMsg", "결재 실패");
             return "dayoff/adminDayoff";
         }
-        
-        
+
     }
-    
-    
-    @GetMapping("calendar")
-    public String calendar() {
-        return "dayoff/dayoffCal";
-    }
-    
+
 //    @GetMapping("calendar")
-//    public ModelAndView calendar(ModelAndView mv, HttpServletRequest req) {
-//        
-//        String viewpage = "calendar";
-//        List<Calendar> calendar = null;
-//        try {
-//            calendar = calendarService.getCalendar();
-//            request.setAttribute("calendarList", calendar);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        mv.setViewName(viewpage);
-//        return mv;
+//    public String calendar() {
+//        return "dayoff/dayoffCal";
 //    }
+
+    @GetMapping("calendar")
+    public ModelAndView getCalendar(ModelAndView mv, HttpServletRequest req, DayoffVo vo, HttpSession session) {
+
+        // 로그인 유저 정보 vo에 저장
+        EmpVo loginMember = (EmpVo) session.getAttribute("loginMember");
+        vo.setECode(loginMember.getEmpCode());
+
+        // 사원의 부서 정보 vo에 저장
+        DayoffVo deptVo = service.getDeptVo(vo);
+        vo.setDeptCode(deptVo.getDeptCode());
+
+        String viewpage = "dayoff/dayoffCal";
+        List<DayoffVo> calendar = null;
+        try {
+            calendar = service.getCalendar(vo);
+            req.setAttribute("calendarList", calendar);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mv.setViewName(viewpage);
+        return mv;
+    }
 
 }
--- ws/EveryWare/src/main/webapp/WEB-INF/views/dayoff/dayoffCal.jsp
+++ ws/EveryWare/src/main/webapp/WEB-INF/views/dayoff/dayoffCal.jsp
@@ -1,5 +1,10 @@
+<%@page import="com.kh.app00.dayoff.vo.DayoffVo"%>
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -39,6 +44,7 @@
 		var calendarEl = document.getElementById('calendar');
 		var calendar = new FullCalendar.Calendar(calendarEl, {
 			initialView : 'dayGridMonth', // 초기 로드 될때 보이는 캘린더 화면(기본 설정: 달)
+			locale: 'ko', // 한국어 설정
 			height: 640,//캘린더 크기 설정
 			headerToolbar : { // 헤더에 표시할 툴 바
 				start : 'prev next today',
@@ -53,7 +59,23 @@
 			droppable : true,
 			editable : true,
 			nowIndicator: true, // 현재 시간 마크
-			locale: 'ko' // 한국어 설정
+			events : [ 
+
+    	    <%List<DayoffVo> calendarList = (List<DayoffVo>) request.getAttribute("calendarList");%>
+				
+            <%if (calendarList != null) {%>
+            <%for (DayoffVo vo : calendarList) {%>
+            {
+            	title : '<%=vo.getECode()%>',
+                start : '<%=vo.getOffStartDate()%>',
+                end : '<%=vo.getOffEndDate()%>',
+                color : '#' + Math.round(Math.random() * 0xffffff).toString(16),
+				allDay : true
+             },
+	<%}
+}%>			]
+			
+			
 		});
 		calendar.render();
 	});
