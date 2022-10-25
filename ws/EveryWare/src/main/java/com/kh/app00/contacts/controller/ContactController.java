f7114cb98d97e2e479c544250f316d85b1fdc0cd
DESKTOP-2AF322B\user <jamong>
2022-10-25 21:32:43 +0900

    주소록 목록 수정 / conf 파일 alias 추가

--- ws/EveryWare/src/main/java/com/kh/app00/contacts/controller/ContactController.java
+++ ws/EveryWare/src/main/java/com/kh/app00/contacts/controller/ContactController.java
@@ -1,18 +1,44 @@
 package com.kh.app00.contacts.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.app00.commute.service.CommuteService;
+import com.kh.app00.contacts.service.ContactsService;
+import com.kh.app00.contacts.vo.ContactsVo;
 
 @Controller
 @RequestMapping("contacts")
 public class ContactController {
 
+	private final ContactsService service;
+	
+	@Autowired
+	public ContactController(ContactsService service) {
+		this.service = service;
+	}
+
+	
 	//주소록 목록
 	@GetMapping("contactList")
-	public String contactList() {
+	public String contactList(Model model) {
+		
+		List<ContactsVo> cList = service.selectList();
+		
+		model.addAttribute("cList" , cList);
+		
+		//System.out.println(cList);
+		
 		return "contacts/contactList";
 	}
+	
+	
+	
 	
 	
 }
